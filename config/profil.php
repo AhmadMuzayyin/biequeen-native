1435e06cc6b653d6385d4a294fe9cc47bef9f50e
root <root@srv422195.hstgr.cloud>
2024-08-09 14:28:27 +0000

    Fix bugs

--- config/profil.php
+++ config/profil.php
@@ -7,29 +7,74 @@ if (!isset($_SESSION['user_id'])) {
 }
 // Create Voucher
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_profil'])) {
-    $kode = sanitize_input($_POST['kode']);
-    $tanggal_berakhir = sanitize_input($_POST['tanggal_berakhir']);
-    $nominal = sanitize_input($_POST['nominal']);
-    $data = compact('kode', 'tanggal_berakhir', 'nominal');
-    $errors = validate_profil_data($data);
-    if (empty($errors)) {
-        unset($_SESSION['errors']);
-        $created_at = date("Y-m-d H:i:s");
-        $updated_at = date("Y-m-d H:i:s");
-
-        $stmt = $conn->prepare("INSERT INTO vouchers (kode, tanggal_berakhir, nominal, created_at, updated_at) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssiss", $kode, $tanggal_berakhir, $nominal, $created_at, $updated_at);
-
-        if ($stmt->execute()) {
-            $errors[] = "Voucher created successfully.";
+    try {
+        $nama = sanitize_input($_POST['nama']);
+        $email = sanitize_input($_POST['email']);
+        $alamat = sanitize_input($_POST['alamat']);
+        $logo = sanitize_input($_POST['logo']);
+        $deskripsi = sanitize_input($_POST['deskripsi']);
+        $whatsapp = sanitize_input($_POST['whatsapp']);
+        $facebook = sanitize_input($_POST['facebook']);
+        $instagram = sanitize_input($_POST['instagram']);
+        $qris_pembayaran = sanitize_input($_POST['qris_pembayaran']);
+        $data = compact('nama', 'email', 'alamat', 'logo', 'deskripsi', 'whatsapp', 'facebook', 'instagram', 'qris_pembayaran');
+        $errors = validate_profil_data($data);
+        if (!empty($_FILES['logo']['name'])) {
+            $file_name = $_FILES['logo']['name'];
+            $file_tmp = $_FILES['logo']['tmp_name'];
+            $file_type = $_FILES['logo']['type'];
+            $file_size = $_FILES['logo']['size'];
+            $file_error = $_FILES['logo']['error'];
+            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+            $hashed_name = md5(uniqid('', true)) . '.' . $file_ext;
+            $target_dir = "uploads/";
+            $target_file = $target_dir . $hashed_name;
+            if (move_uploaded_file($file_tmp, $target_file)) {
+                $logo = "/config/$target_file";
+            } else {
+                $errors[] = "Sorry, there was an error uploading your file.";
+            }
         } else {
-            $errors[] = "Error: " . $stmt->error;
+            $logo = $profil['logo'];
         }
-        $stmt->close();
-        header("Location: /admin/promo");
-    } else {
-        $_SESSION['errors'] = $errors;
-        header("Location: /admin/promo/create");
+
+        if (!empty($_FILES['qris_pembayaran']['name'])) {
+            $file_name = $_FILES['qris_pembayaran']['name'];
+            $file_tmp = $_FILES['qris_pembayaran']['tmp_name'];
+            $file_type = $_FILES['qris_pembayaran']['type'];
+            $file_size = $_FILES['qris_pembayaran']['size'];
+            $file_error = $_FILES['qris_pembayaran']['error'];
+            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+            $hashed_name = md5(uniqid('', true)) . '.' . $file_ext;
+            $target_dir = "uploads/";
+            $target_file = $target_dir . $hashed_name;
+            if (move_uploaded_file($file_tmp, $target_file)) {
+                $qris_pembayaran = "/config/$target_file";
+            } else {
+                $errors[] = "Sorry, there was an error uploading your file.";
+            }
+        } else {
+            $qris_pembayaran = $profil['qris_pembayaran'];
+        }
+
+        if (empty($errors)) {
+            unset($_SESSION['errors']);
+            $stmt = $conn->prepare("INSERT INTO company_profiles (nama, email, alamat, logo, deskripsi, whatsapp, facebook, instagram, qris_pembayaran) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssssssss", $nama, $email, $alamat, $logo, $deskripsi, $whatsapp, $facebook, $instagram, $qris_pembayaran);        
+            if ($stmt->execute()) {
+                $errors[] = "Profil created successfully.";
+            } else {
+                $errors[] = "Error: " . $stmt->error;
+            }
+            $stmt->close();
+            header("Location: /admin/profil");
+        } else {
+            $_SESSION['errors'] = $errors;
+            header("Location: /admin/profil");
+        }
+    } catch (\Throwable $th) {
+        var_dump($th->getMessage());
+        die();
     }
 }
 // Update company profile
